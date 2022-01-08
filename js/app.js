d72a2fc38d085046e5ca5adc3e24e46ab3c94163
TravisAlstrand <travisalstrand@gmail.com>
2022-01-08 13:16:55 -0800

    Commit 8
    Alert box fixed and toggle switch colors fixed. Started first chart, isn't showing up on page yet.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const alertBanner = document.getElementById("alert");
 
+const trafficCanvas = document.getElementById("traffic-chart");
+
 alertBanner.innerHTML = `
     <div class="alert-banner">
         <p><strong>Alert:</strong> You have unread messages.</p>
@@ -7,9 +9,45 @@ alertBanner.innerHTML = `
     </div>
 `;
 
-addEventListener.alertBanner('click', e => {
+alertBanner.addEventListener('click', (e) => {
     const element = e.target;
-    if (element.ClassList.contains('alert-banner-close')) {
+    if (element.classList.contains("alert-banner-close")) {
         alertBanner.style.display = 'none';
     }
+});
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+        "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+            2500],
+        backgroundColor: 'rgba(139, 152, 214, .3)',
+        borderWidth: 1
+    }]
+};
+
+let trafficOptions = {
+    backgroundColor: 'rgba(139, 152, 214, .3)',
+    fill: true,
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
 });
