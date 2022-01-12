76d036e68e687f715674e31aefa5663b1dd73c5e
TravisAlstrand <travisalstrand@gmail.com>
2022-01-11 16:57:49 -0800

    Commit 9
    Charts implemented, just needs line chart to change with traffic-nav.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const alertBanner = document.getElementById("alert");
 
 const trafficCanvas = document.getElementById("traffic-chart");
+const dailyCanvas = document.getElementById("daily-chart");
+const mobileCanvas = document.getElementById("mobile-chart");
+
+// ALERT BANNER
 
 alertBanner.innerHTML = `
     <div class="alert-banner">
@@ -16,6 +20,8 @@ alertBanner.addEventListener('click', (e) => {
     }
 });
 
+// LINE CHART
+
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
         "4-10", "11-17", "18-24", "25-31"],
@@ -23,12 +29,14 @@ let trafficData = {
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
             2500],
         backgroundColor: 'rgba(139, 152, 214, .3)',
-        borderWidth: 1
+        borderWidth: 1,
+        cubicInterpolationMode: 'monotone',
+        pointHoverBackgroundColor: 'rgb(111, 199, 135)',
+        pointHoverRadius: 7
     }]
 };
 
 let trafficOptions = {
-    backgroundColor: 'rgba(139, 152, 214, .3)',
     fill: true,
     aspectRatio: 2.5,
     animation: {
@@ -50,4 +58,73 @@ let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
     data: trafficData,
     options: trafficOptions
+});
+
+// BAR CHART
+
+let dailyData = {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets: [{
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: 'rgb(139, 152, 214)',
+        borderWidth: 1
+    }]
+};
+
+let dailyOptions = {
+    fill: true,
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let barChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+// DONUT CHART
+
+let mobileData = {
+    labels: ["Desktop", "Tablets", "Phones"],
+    datasets: [{
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            'rgb(139, 152, 214)',
+            'rgb(111, 199, 135)',
+            'rgb(61, 203, 235)'
+        ]
+    }]
+};
+
+let mobileOptions = {
+    aspectRatio: 1.9,
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 30,
+                fontStyle: 'bold'
+            }
+        }
+    }
+};
+
+let doughnutChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
 });
